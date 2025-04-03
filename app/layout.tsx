27b569bc886526dc6aa51a4a4cebd6f0cac7d53f
Van Kaizer Sebastian <vk.aizn@gmail.com>
2025-04-03 10:48:11 +0800

    Test commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`antialiased`}>
+            <body>
                 <Navigation />
                 {children}
             </body>
