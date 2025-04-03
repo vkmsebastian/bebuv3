172bd22034f82e71ff8103cfbc35461c49feb3c8
Van Kaizer Sebastian <vk.aizn@gmail.com>
2025-04-03 16:05:14 +0800

    Title text env test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 'use client';
+import { useState, useEffect } from 'react';
 
-import { Fredericka_the_Great, Alumni_Sans_Pinstripe } from 'next/font/google';
-import Image from 'next/image';
 import Link from 'next/link';
-import './globals.css';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { Love_Light, Alumni_Sans_Pinstripe, Fredericka_the_Great } from 'next/font/google';
+
+import './globals.css';
 
 const fredericka = Fredericka_the_Great({
     subsets: ['latin'],
@@ -18,6 +20,12 @@ const alumniSans = Alumni_Sans_Pinstripe({
     weight: '400',
 });
 
+const mainTitle = Love_Light({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400',
+});
+
 const links = [
     { href: '/', title: 'Home' },
     { href: '/player', title: 'Player' },
@@ -25,21 +33,42 @@ const links = [
     { href: '/about', title: 'About' },
 ];
 
+const titleText = process.env.NEXT_PUBLIC_TITLE_TEXT;
+
 function NavLink({ title, href }: { title: string; href: string }) {
     const pathname = usePathname();
     const isActive = pathname === href;
     return (
-        <Link href={href} className={`${isActive ? 'text-blue-600 font-bold' : ''} hover:text-blue-300`}>
+        <Link href={href} className={`${isActive ? 'font-bold text-lg' : ''} hover:text-blue-300`}>
             {title}
         </Link>
     );
 }
 
 function Navigation() {
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme');
+        const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const initialMode = storedTheme ? storedTheme === 'dark' : systemDark;
+
+        setIsDarkMode(initialMode);
+        document.documentElement.classList.toggle('dark', initialMode);
+    }, []);
+
+    const toggleDarkMode = () => {
+        const newMode = !isDarkMode;
+        setIsDarkMode(newMode);
+        document.documentElement.classList.toggle('dark', newMode);
+        localStorage.setItem('theme', newMode ? 'dark' : 'light');
+    };
+
     return (
-        <div className="border-b border-gray-600 flex sm:flex-row flex-col sm:justify-between sm:py-3 ">
+        <div className="border-b border-gray-600 flex sm:flex-row flex-col sm:justify-between sm:py-3 sm:px-5">
             <div className="flex flex-row items-center justify-center px-3 my-3 sm:my-0">
-                <p className={`${fredericka.className} text-6xl px-5`}>VS||JS</p>
+                <p className={`${mainTitle.className} text-6xl`}>{titleText}</p>
                 <p className={`${alumniSans.className} text-2xl`}>Year 3</p>
             </div>
             <div className="sm:w-1/3 flex sm:justify-between">
@@ -48,8 +77,17 @@ function Navigation() {
                         <NavLink key={link.href} {...link} />
                     ))}
                 </div>
-                <div className="flex items-center px-2">
-                    <Image src="/images/cat.png" width={50} height={50} alt="logo" />
+                <div className="flex items-center px- w-[60px]">
+                    <button onClick={toggleDarkMode}>
+                        <Image
+                            src={`/images/${isDarkMode ? 'dark-mode' : 'light-mode'}.png`}
+                            width={60}
+                            height={60}
+                            alt="logo"
+                            onLoad={() => setIsImageLoaded(true)}
+                            className={`${isImageLoaded ? '' : 'img-spinner'}`}
+                        />
+                    </button>
                 </div>
             </div>
         </div>
@@ -63,7 +101,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="antialiased">
+            <body className="bg-white dark:bg-black dark:text-white antialiased">
                 <Navigation />
                 {children}
             </body>
