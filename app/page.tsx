be2f126900e8f84328ef309053057e4c2786b07b
Van Kaizer Sebastian <vk.aizn@gmail.com>
2025-04-13 20:33:38 +0800

    Remove home metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "BebuV3",
-  description: "3rd Year Anniversary",
-};
-
 export default function Home() {
   return "Home";
 }
